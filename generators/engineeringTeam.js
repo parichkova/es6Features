d400a1d0d5b0886a4ce8d6d2db3d11f33f0b45a4
parichkova <Tihomira.Parichkova@fourth.com>
2018-11-27 00:19:24 +0200

    generators

--- generators/engineeringTeam.js
+++ generators/engineeringTeam.js
@@ -1,16 +1,29 @@
+const testingTeam = {
+    lead: 'Amanda',
+    tester: 'Bill'
+};
 const engineeringTeam = {
+    testingTeam,
     size: 3,
     department: 'Engineering',
     lead: 'Jill',
     qa: 'Alex',
-    dev: 'Jason'
+    dev: 'Dave'
 };
 
+function* teamIterator(team) {
+    yield team.lead;
+    yield team.qa;
+    yield team.dev;
 
-function* teamIterator(engineeringTeam) {
-    yield engineeringTeam.lead;
-    yield engineeringTeam.qa;
-    yield engineeringTeam.dev;
+    const testingTeamGenerator = testingTeamIterator(team.testingTeam);
+    
+    yield* testingTeamGenerator;
+}
+
+function* testingTeamIterator(team) {
+    yield team.lead;
+    yield team.tester;
 }
 
 const names = [];
